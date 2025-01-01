c39329b817a3df539e3b42313d83f635c58c6ef5
xOcarin <cschilling16111@gmail.com>
2025-01-01 15:05:58 -0500

    placeholders

--- src/App.js
+++ src/App.js
@@ -4,19 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div id='piecesDiv'>
-          <img className='piecesimg' src="/mii.png" alt="Description" />
-          <img className='piecesimg' src="/mii.png" alt="Description" />
-          <img className='piecesimg' src="/mii.png" alt="Description" />
-          <img className='piecesimg' src="/mii.png" alt="Description" />
-          <img className='piecesimg' src="/mii.png" alt="Description" />
-        </div>
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
-        <img className='shelfimg' src="/shelf2.png" alt="Description" />
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
+
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
+
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
+
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
+
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
+
+      <div id='piecesDiv'>
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+        <img className='piecesimg' src="/mii.png" alt="Description" />
+      </div>
+      <img className='shelfimg' src="/shelf2.png" alt="Description" />
     </div>
   );
 }
